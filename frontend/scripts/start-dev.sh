2e1b4cfb47b4ebffbfc4bccdbeb73b139ff58381
FrazaAdityaWiguna <adityafraja12@gmail.com>
2023-09-21 21:49:24 +0700

    frontend-feat: setup husky

---
+++ frontend/scripts/start-dev.sh
@@ -0,0 +1,19 @@
+echo "<<< husky setup"
+
+cd ..  && npm run prepare
+
+echo "### successfully husky setup"
+
+cd frontend
+
+while ! [ -f "$ENVFILE" ]; do
+    read -p "Invalid ENV File directory, Please make sure your env project"
+done
+
+set -a
+. $ENVFILE
+set +a
+
+
+echo "Runing the project..."
+next start
